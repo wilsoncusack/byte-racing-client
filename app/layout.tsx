5e74768b784710831baa5691debaf5caab71c2c5
Wilson Cusack <cusackw@gmail.com>
2024-07-06 12:04:44 -0400

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import { pressStart2P } from "./lib/fonts";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={pressStart2P.className}>
+      <Head>
+          <script defer src="https://api.pirsch.io/pa.js"
+        id="pianjs"
+        data-code="auPvWdXpZYJMo8FlQXSmtNx5SiQQ9F3G"></script>
+      </Head>
       <body className={inter.className}>{children}</body>
       <Analytics />
     </html>
